c6f6084fc1a86d5be4c9f5dc2e8655032fa87652
Jakub Duzyk <jakub.duzyk@lumesse.com>
2018-04-21 15:55:03 +0200

    Endless road first pass

--- Traffic/Main.cpp
+++ Traffic/Main.cpp
@@ -1,17 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+const int speed = 15;
+const int scene_width = 640;
+const int scene_height = 1024;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600),
+	sf::RenderWindow window(sf::VideoMode(scene_width, scene_height),
 		"Hello SFML", sf::Style::Default);
 
 	sf::Font font;
 	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
 
-	sf::Text text;
-	text.setFont(font);
-	text.setPosition(200, 200);
-	text.setString("Hello SFML");
+	sf::Texture texture;
+	if (!texture.loadFromFile("./road.jpg"))
+	{
+		std::cout << "Error loading image";
+	}
+	sf::Sprite sprite1, sprite2;
+	sprite1.setTexture(texture);
+	sprite2.setTexture(texture);
+
+	int height = sprite1.getLocalBounds().height;
+	sprite2.setPosition(sf::Vector2f(0.0, -height));
+
+	window.setFramerateLimit(60);
 
 	while (window.isOpen())
 	{
@@ -22,7 +36,18 @@ int main()
 				window.close();
 		}
 		window.clear();
-		window.draw(text);
+
+		sprite1.move(sf::Vector2f(0, speed));
+		sprite2.move(sf::Vector2f(0, speed));
+		window.draw(sprite1);
+		window.draw(sprite2);
+
+		if (sprite1.getPosition().y >= scene_height) {
+			sprite1.setPosition(sf::Vector2f(0, sprite2.getPosition().y - height));
+		}
+		if (sprite2.getPosition().y >= scene_height) {
+			sprite2.setPosition(sf::Vector2f(0, sprite1.getPosition().y - height));
+		}
 		window.display();
 	}
 
